41a52f9eb59fa7cb11c199a69274d79255ea125e
LewisJassy <jassylewis70@gmail.com>
2024-02-28 22:14:48 -0800

    remove trailling whitespaces

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,12 +9,12 @@
  * Return: pointer to new node OR (NULL) on failure
  */
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) 
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 
     binary_tree_t *newNode =  (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
-    if (newNode == NULL) 
+    if (newNode == NULL)
     {
         return (NULL);
     }
