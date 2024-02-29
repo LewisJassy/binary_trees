c072f9fcea2a0a4eba11b89489cff8b27ba9a115
LewisJassy <jassylewis70@gmail.com>
2024-03-01 00:52:55 +0300

    update the code

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,20 +1,46 @@
-#include <stdio.h>
 #include "binary_trees.h"
+#include <stdlib.h>
 
+binary_tree_t *create_empty_node(int value);
+
+/**
+ * binary_tree_insert_left - inserts a node to the left of parent
+ * @parent: parent node to insert child under
+ * @value: value to create new node data
+ *
+ * Return: pointer to new binary tree node
+ */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node = NULL;
 
-    if (newNode == NULL)
-    {
-        return (NULL);
-    }
+	if (!parent)
+		return (NULL);
 
-    newNode->n = value;
-    newNode->parent = parent;
-    
-    if (parent != NULL && parent->left != NULL)
-    {
+	new_node = create_empty_node(value);
+	if (!new_node)
+		return (NULL);
+	new_node->parent = parent;
+
+	/* parent contains left node, must be shifted */
+	if (parent->left)
+	{
+		new_node->left = parent->left;
+		new_node->left->parent = new_node;
+	}
+	parent->left = new_node;
+
+	return (new_node);
+}
+
+/**
+ * create_empty_node - creates a node with NULL pointers and a value
+ * @value: value to set as n of node
+ *
+ * Return: newly created orphan node
+ */
+binary_tree_t *create_empty_node(int value)
+{
 	binary_tree_t *new_node = NULL;
 
 	new_node = malloc(sizeof(binary_tree_t));
