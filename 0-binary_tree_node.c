424fee1b0d2f546b8700507e61ab79a2fcab8cf5
LewisJassy <jassylewis70@gmail.com>
2024-02-29 08:59:03 +0300

    remove trailling white space

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,14 +11,14 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) 
 {
-    // Allocate new memory for the new node
+    
     binary_tree_t *newNode =  (binary_tree_t *)malloc(sizeof(binary_tree_t));
-    // check if the memory allocation was succesfull
+    
     if (newNode == NULL) 
     {
         return (NULL);
     }
-    // set values for the node
+    
     newNode->n = value;
     newNode->parent = parent;
     newNode->right = NULL;
